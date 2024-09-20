6f6de304d66b67844f99453e8564d39b9aa0853a
azim <mdazimmia53@gmail.com>
2024-09-20 14:26:21 +0600

    change

--- src/naveheader/navlink.jsx
+++ src/naveheader/navlink.jsx
@@ -6,7 +6,7 @@ const Navlink=()=> {
   <header>
   <nav>
   <ul className="flex">
- <li className="link"><Link to="/news-protfolio">Home</Link></li> 
+ <li className="link"><Link to="/">Home</Link></li> 
  <li className="link"><Link to="/education">Education</Link></li> 
   <li className="link"><Link to="/sport">Sport</Link></li> 
  <li className="link"><Link to="/nu/notice">Nu/notice</Link></li> 
