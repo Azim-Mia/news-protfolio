6125244120dddd1a27ae6276e8cea989a75fd9a4
azim <mdazimmia53@gmail.com>
2024-09-20 17:43:02 +0600

    modify

--- src/naveheader/navlink.jsx
+++ src/naveheader/navlink.jsx
@@ -1,10 +1,15 @@
 import {Link ,Outlet} from "react-router-dom";
 import "./nav.css";
-//import MobileNav from './mobileNav.jsx';
+import MobileNav from './mobileNav.jsx';
+import UpperNav from './upperNav/upperNav'
 const Navlink=()=> {
   return ( <>
   <header>
-  <nav>
+  <div><UpperNav /></div>
+ <div className="md:dis_icon_none lg:dis_icon_none lg:dis_icon_none">
+   <MobileNav />
+  </div>
+  <nav className=" mt-2 ssm:rs_dis_none sm:respon_sm md:respon_md lg:respon_lg ">
   <ul className="flex">
  <li className="link"><Link to="/news-protfolio">Home</Link></li> 
  <li className="link"><Link to="/education">Education</Link></li> 
@@ -14,7 +19,7 @@ const Navlink=()=> {
  <li className="link"><Link to="/international">International</Link></li> 
   </ul>
 </nav>
-<Outlet />
+<div className="mt-2"><Outlet /></div>
 </header>
 </>)
 }
